cdc3d693a23d2a93e504c6f3b7c4b9252569260c
mubeen-afzal <149320283+mubeen-afzal@users.noreply.github.com>
2025-02-11 18:04:55 +0500

    dynamic image handling

---
+++ assets/img/product_data.js
@@ -0,0 +1,192 @@
+// data.js
+const myData = [
+    {
+        "number": 1,
+        "h4": "Product 1",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/1.jpg",
+        "href": "assets/img/portfolio/Pliers/1.jpg",
+        "title": "Product 1",
+        "filter": "filter-spreader"
+    },
+    {
+        "number": 2,
+        "h4": "Product 2",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/2.jpg",
+        "href": "assets/img/portfolio/Pliers/2.jpg",
+        "title": "Product 2",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 3,
+        "h4": "Product 3",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/3.jpg",
+        "href": "assets/img/portfolio/Pliers/3.jpg",
+        "title": "Product 3",
+        "filter": "filter-spreader"
+    },
+    {
+        "number": 4,
+        "h4": "Product 4",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/4.jpg",
+        "href": "assets/img/portfolio/Pliers/4.jpg",
+        "title": "Product 4",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 5,
+        "h4": "Product 5",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/5.jpg",
+        "href": "assets/img/portfolio/Pliers/5.jpg",
+        "title": "Product 5",
+        "filter": "filter-spreader"
+    },
+    {
+        "number": 6,
+        "h4": "Product 6",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/6.jpg",
+        "href": "assets/img/portfolio/Pliers/6.jpg",
+        "title": "Product 6",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 7,
+        "h4": "Product 7",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/7.jpg",
+        "href": "assets/img/portfolio/Pliers/7.jpg",
+        "title": "Product 7",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 8,
+        "h4": "Product 8",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/8.jpg",
+        "href": "assets/img/portfolio/Pliers/8.jpg",
+        "title": "Product 8",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 9,
+        "h4": "Product 9",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/9.jpg",
+        "href": "assets/img/portfolio/Pliers/9.jpg",
+        "title": "Product 9",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 10,
+        "h4": "Product 10",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/10.jpg",
+        "href": "assets/img/portfolio/Pliers/10.jpg",
+        "title": "Product 10",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 11,
+        "h4": "Product 11",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/11.jpg",
+        "href": "assets/img/portfolio/Pliers/11.jpg",
+        "title": "Product 11",
+        "filter": "filter-spreader"
+    },
+    {
+        "number": 12,
+        "h4": "Product 12",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/12.jpg",
+        "href": "assets/img/portfolio/Pliers/12.jpg",
+        "title": "Product 12",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 13,
+        "h4": "Product 13",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/13.jpg",
+        "href": "assets/img/portfolio/Pliers/13.jpg",
+        "title": "Product 13",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 14,
+        "h4": "Product 14",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/14.jpg",
+        "href": "assets/img/portfolio/Pliers/14.jpg",
+        "title": "Product 14",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 15,
+        "h4": "Product 15",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/15.jpg",
+        "href": "assets/img/portfolio/Pliers/15.jpg",
+        "title": "Product 15",
+        "filter": "filter-spreader"
+    },
+    {
+        "number": 16,
+        "h4": "Product 16",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/16.jpg",
+        "href": "assets/img/portfolio/Pliers/16.jpg",
+        "title": "Product 16",
+        "filter": "filter-dental"
+    },
+    {
+        "number": 17,
+        "h4": "Product 17",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/17.jpg",
+        "href": "assets/img/portfolio/Pliers/17.jpg",
+        "title": "Product 17",
+        "filter": "filter-spreader"
+    },
+    {
+        "number": 18,
+        "h4": "Product 18",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/18.jpg",
+        "href": "assets/img/portfolio/Pliers/18.jpg",
+        "title": "Product 18",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 19,
+        "h4": "Product 19",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/19.jpg",
+        "href": "assets/img/portfolio/Pliers/19.jpg",
+        "title": "Product 19",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 20,
+        "h4": "Product 20",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/20.jpg",
+        "href": "assets/img/portfolio/Pliers/20.jpg",
+        "title": "Product 20",
+        "filter": "filter-plier"
+    },
+    {
+        "number": 21,
+        "h4": "Product 21",
+        "p": "details of product",
+        "src": "assets/img/portfolio/Pliers/21.jpg",
+        "href": "assets/img/portfolio/Pliers/21.jpg",
+        "title": "Product 21",
+        "filter": "filter-surgical"
+    },
+]
